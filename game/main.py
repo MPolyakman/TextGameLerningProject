466f560b036c02e74d24765e4fbf348c8c859352
vlad0 <vlad030306456@gmail.com>
2025-07-27 17:54:43 +0700

    fix

--- game/main.py
+++ game/main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QVBoxLayout
 from PySide6.QtCore import Qt, QSize
 
 class MainWindow(QMainWindow):
@@ -9,14 +9,33 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(144,144))
         self.setMaximumSize(QSize(1080, 740))
         self.resize(1080, 740)
-
+        
+        # кнопка выхода
         exitbutton = QPushButton('EXIT')
         exitbutton.setCheckable(True)
         exitbutton.setFixedSize(100,100)
         exitbutton.clicked.connect(self.close_the_game)
 
-        self.setCentralWidget(exitbutton)
+        # кнопка игрулькать
+        newgame_button = QPushButton('New Gay')
+        newgame_button.setCheckable(True)
+        newgame_button.setFixedSize(100,100)
+        # newgame_button.clicked.connect(self.close_the_game)
 
+        # self.setCentralWidget(exitbutton)
+
+        #менюха
+        main_menu_bar = QWidget()
+        self.setCentralWidget(main_menu_bar)
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.setSpacing(30)
+
+        layout.addWidget(newgame_button)
+        layout.addWidget(exitbutton)
+
+        main_menu_bar.setLayout(layout)
 
 
     def close_the_game(self):
