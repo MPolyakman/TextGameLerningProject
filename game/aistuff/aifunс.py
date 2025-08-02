a3d66e7ec5ea3e544a4924730a6dd7bc2bd34a12
vlad0 <vlad030306456@gmail.com>
2025-08-02 21:41:39 +0700

    red prompt

--- game/aistuff/aifunс.py
+++ game/aistuff/aifunс.py
@@ -4,6 +4,6 @@ def fixmes(mes: str) -> str:
     red_mes = ollama.chat(
     model='llama3:instruct',
     messages=[
-        {'role': 'user', 'content': f'Исправь это сообщение в соответсвии со всеми нормами русского языка: {mes}'}
+        {'role': 'user', 'content': f'Исправь ошибки, чтобы звучало нормально и больше ничего не пиши: {mes}'}
     ])
-    return red_mes['messages']['content']
+    return red_mes['message']['content']
